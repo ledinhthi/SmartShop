959417f5aa4bf4a140577c4c90aa7c2e5a7c7fe3
thild2 <ledinhthi2@gmail.com>
2021-03-28 01:06:32 +0700

    Init project

---
+++ App.js
@@ -0,0 +1,31 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ * Redux, navigation
+ */
+
+import React from 'react';
+import {
+ 
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return (
+    <SafeAreaView style={backgroundStyle}>
+     
+    </SafeAreaView>
+  );
+};
+
+export default App;
